4d9c954f5eb1a61c5431ddaa66bce84b94db0190
Thomas Lim <txl11@ic.ac.uk>
2015-02-11 22:09:42 +0000

    Task 5 complete - adjustable grain size before calling tab::task_group

--- src/txl11/fast_fourier_transform_taskgroup.cpp
+++ src/txl11/fast_fourier_transform_taskgroup.cpp
@@ -13,6 +13,9 @@ namespace txl11 {
 class fast_fourier_transform_taskgroup
     : public fourier_transform
 {
+private:
+    mutable unsigned recursion_size = 0;
+
 protected:
     /* Standard radix-2 FFT only supports binary power lengths */
     virtual size_t calc_padded_size(size_t n) const
@@ -27,6 +30,25 @@ protected:
         return ret;
     }
 
+    virtual size_t get_recursion_size(void) const
+    {
+        // printf("In get_recursion_size. val is %i\n", recursion_size);
+        if (recursion_size != 0){
+            return recursion_size;
+        }
+
+        char *v=getenv("HPCE_FFT_RECURSION_K");
+        if(v==NULL){
+            recursion_size = 16;
+            printf("HPCE_FFT_RECURSION_K not set. Using a size of %i instead.\n", recursion_size);
+            
+        }else{
+            recursion_size = atoi(v);
+            printf("Using a chunk size of %i (set in the environment variable 'HPCE_FFT_RECURSION_K'.\n)", recursion_size);
+        }
+        return recursion_size;
+    }
+
     virtual void forwards_impl(
         size_t n,   const std::complex<double> &wn,
         const std::complex<double> *pIn, size_t sIn,
@@ -34,13 +56,33 @@ protected:
     ) const 
     {
         assert(n>0);
+
+        unsigned K = get_recursion_size();
         
         if (n == 1){
             pOut[0] = pIn[0];
         }else if (n == 2){
             pOut[0] = pIn[0]+pIn[sIn];
             pOut[sOut] = pIn[0]-pIn[sIn];
+        }else if (n <= K){
+            // printf("less than K, so not task grouping. n=%i\n", n);
+            size_t m = n/2;
+
+            forwards_impl(m,wn*wn,pIn,2*sIn,pOut,sOut);
+            forwards_impl(m,wn*wn,pIn+sIn,2*sIn,pOut+sOut*m,sOut);
+
+            std::complex<double> w=std::complex<double>(1.0, 0.0);
+
+            for (size_t j=0;j<m;j++){
+              std::complex<double> t1 = w*pOut[m+j];
+              std::complex<double> t2 = pOut[j]-t1;
+              pOut[j] = pOut[j]+t1;                 /*  pOut[j] = pOut[j] + w^i pOut[m+j] */
+              pOut[j+m] = t2;                          /*  pOut[j] = pOut[j] - w^i pOut[m+j] */
+              w = w*wn;
+            }
+
         }else{
+            // printf("greater than K, so task grouping. n=%i\n", n);
             size_t m = n/2;
 
             tbb::task_group group;
