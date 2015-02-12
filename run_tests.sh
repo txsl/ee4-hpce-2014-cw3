8a2d3ca23b6eb42b52221bdaa8040e9d383e17ad
Thomas Lim <txl11@ray06.doc.ic.ac.uk>
2015-02-12 23:25:55 +0000

    Added opt to run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,7 @@
 
 TASKS="hpce.fast_fourier_transform hpce.direct_fourier_transform hpce.YOUR_LOGIN.direct_fourier_transform_parfor"
 TASKS="${TASKS} hpce.txl11.direct_fourier_transform_parfor hpce.txl11.fast_fourier_transform_taskgroup hpce.txl11.direct_fourier_transform_chunked" 
-TASKS="${TASKS} hpce.txl11.fast_fourier_transform_parfor hpce.txl11.fast_fourier_transform_combined"
+TASKS="${TASKS} hpce.txl11.fast_fourier_transform_parfor hpce.txl11.fast_fourier_transform_combined hpce.txl11.fast_fourier_transform_opt"
 
 for t in $TASKS; do
     echo "Running test for ${t}";
